fe94c55f56331e5941029581e0f93d765002256b
n-tsukiya <tkmaru555@gmail.com>
2022-10-28 18:03:01 +0900

    セクション12-69: Globalスタイルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./App.css";
 import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>あああああああああああああ</p>
     </ChakraProvider>
   );
 }
